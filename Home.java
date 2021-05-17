f101e4a24343b6bd4e6a59b8914120d1435c80f4
SachiPU12 <83857134+SachiPU12@users.noreply.github.com>
2021-05-17 22:19:40 +0530

    Add files via upload

---
+++ Home.java
@@ -0,0 +1,7 @@
+public class Home{
+	public static void main(String args[]){
+		System.out.println
+		("\t    *    \t\n\t    |    \t\n\t    |    \t\n\t    |    \t\n\t    |    \t\n\t    /\\   \t\n\t   /  \\  \t\n\t  /    \\ \t\n\t /      \\\t\n\t|--------|\t\n\t|--------|\t\n\t|        |\t\n\t|        |\t\n\t|        |\t\n\t|   ___  |\t\n\t|   | |  |\t\n\t|___|_|__|\t\n");
+		}
+}
+				
